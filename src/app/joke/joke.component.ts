5de02615c322adf16e1969db68915f4a6bb99692
“Jimmy <jimmyabelrojas@gmail.com>
2018-04-12 21:23:51 -0400

    Use joke object for joke component. Set view encapsulation to none.

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-joke',
   templateUrl: './joke.component.html',
+  /*template: `<h4>{{joke.setup}}</h4>
+  <p>
+  {{joke.punchline}}
+  </p>
+  `,*/
   styleUrls: ['./joke.component.css']
 })
 export class JokeComponent implements OnInit {
 
-  joke: string;
+  joke: object;
   constructor() {
-    this.joke = 'What did the cheese say when it looked in the mirror?';
+    this.joke = {
+      setup: 'What did the cheese say when it looked in the mirror?',
+      punchline: 'Halloumi'
+    };
   }
 
   ngOnInit() {
