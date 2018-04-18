672951f9829489536d10b4e5f20288cec8e89d31
“Jimmy <jimmyabelrojas@gmail.com>
2018-04-17 21:24:49 -0400

    Displaying a list of jokes

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Joke} from '../joke';
 
 @Component({
   selector: 'app-joke-list',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JokeListComponent implements OnInit {
 
-  constructor() { }
+  jokes: Joke[];
+  constructor() {
+    this.jokes = [
+      new Joke('1. What did the cheese say when it looked in the mirror?', 'Hello-me (Halloumi)'),
+      new Joke('2. What kind of cheese do you use to disguise a small horse?', 'Mask-a-pony (Mascarpone)'),
+      new Joke('3. A kid threw a lump of cheddar at me', 'I thought ‘That’s not very mature’'),
+    ];
+  }
 
   ngOnInit() {
   }
