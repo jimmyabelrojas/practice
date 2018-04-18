7d9ee5e5d66cd692949fe89022b3a5eba6583f4b
“Jimmy <jimmyabelrojas@gmail.com>
2018-04-17 20:57:53 -0400

    Toggling button text when it is clicked

--- src/app/joke.ts
+++ src/app/joke.ts
@@ -2,14 +2,20 @@ export class Joke {
   setup: string;
   punchline: string;
   hide: boolean;
-
+  buttonText: string;
   constructor(setup: string, punchline: string) {
     this.setup = setup;
     this.punchline = punchline;
     this.hide = true;
+    this.buttonText = 'Show me';
   }
 
   toggle() {
     this.hide = !this.hide;
+    if (this.hide) {
+      this.buttonText = 'Show me';
+    } else {
+      this.buttonText = 'Hide me';
+    }
   }
 }
